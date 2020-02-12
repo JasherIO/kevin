d0296f53e6604e60c9d3e2e87e8652c403458c7e
Willis <3475947+JasherIO@users.noreply.github.com>
2020-02-12 13:25:19 -0600

    Generic auto replies

--- bot.js
+++ bot.js
@@ -4,20 +4,29 @@ const sqlite = require('sqlite');
 
 require('dotenv').config()
 
+const autoReplies = {
+  peen: "`Please think of Stump before waving your peen around`",
+  bing: "`Bong!`"
+}
+
 const client = new Client({
 	owner: process.env.ownerId
 });
 
 client.on('message', message => {
-  if (message.content.includes('peen') && !message.author.bot)
-    message.channel.send("`Please think of Stump before waving your peen around`");
-    
-  if (message.content.includes('bing') && !message.author.bot)
-    message.channel.send("`Bong!`");
+  if (message.author.bot)
+    return;
+
+  Object.keys(autoReplies).forEach((key) => {
+    if (!message.content.includes(key))
+      return;
+
+    message.channel.send(autoReplies[key]);
+  })
 })
 
 client.registry
-  .registerGroups([ [ 'queue', 'Queue' ], [ 'fun', 'Fun' ] ])
+  .registerGroups([ ['queue', 'Queue'], ['fun', 'Fun'] ])
   .registerDefaults()
   .registerCommandsIn(path.join(__dirname, 'commands'));
 
