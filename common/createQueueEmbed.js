0805b7b089124a2312b8164235f946421e8f0a85
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2020-10-22 16:09:10 -0500

    Update fields in-place

--- common/createQueueEmbed.js
+++ common/createQueueEmbed.js
@@ -1,32 +1,45 @@
 const { MessageEmbed } = require('discord.js');
 const { toCentralEuropeanDate, toEasternDate } = require("./dates");
 const { QUEUE_START_COLOR, QUEUE_END_COLOR } = require("./queueColors");
-const  { Q, S, EMOJIS } = require("./emojis");
+const  { C, S } = require("./emojis");
+
 const TWO_WEEKS_MS = 1209600000;
+const EMOJIS = [C, S];
 
 const toMentions = (users) => {
   return users.map(user => `<@${user.id}>`);
 }
 
-const toColor = ({ queueSize, partySize }) => {
-  return queueSize < partySize ? QUEUE_START_COLOR : QUEUE_END_COLOR;
+const toColor = ({ confirmedSize, partySize }) => {
+  return confirmedSize < partySize ? QUEUE_START_COLOR : QUEUE_END_COLOR;
 }
 
 const updateEmbed = ({ template, reactions, partySize }) => {
   const embed = new MessageEmbed(template);
 
-  const queueReaction = reactions.find(r => r.emoji.name === Q);
-  const queueUsers = queueReaction ? queueReaction.users.cache.filter(u => !u.bot) : {};
-  const queueSize = queueUsers.size || 0;
+  const confirmedReaction = reactions.find(r => r.emoji.name === C);
+  const confirmedUsers = confirmedReaction ? confirmedReaction.users.cache.filter(u => !u.bot) : {};
+  const confirmedSize = confirmedUsers.size || 0;
 
   const standbyReaction = reactions.find(r => r.emoji.name === S);
   const standbyUsers = standbyReaction ? standbyReaction.users.cache.filter(u => !u.bot) : {};
   const standbySize = standbyUsers.size || 0;
 
-  embed.setColor(toColor({ queueSize, partySize }));
+  embed.setColor(toColor({ confirmedSize, partySize }));
 
-  embed.addField(`Confirmed (${queueSize})`, queueSize > 0 ? toMentions(queueUsers) : 'None');
-  embed.addField(`Standby (${standbySize})`, standbySize > 0 ? toMentions(standbyUsers) : 'None');
+  const confirmedFieldIndex = embed.fields.findIndex(f => f.name.startsWith('Confirmed '));
+  embed.fields[confirmedFieldIndex] = {
+    ...embed.fields[confirmedFieldIndex],
+    name: `Confirmed (${confirmedSize})`,
+    value: confirmedSize > 0 ? toMentions(confirmedUsers) : 'None'
+  }
+
+  const standbyFieldIndex = embed.fields.findIndex(f => f.name.startsWith('Standby '));
+  embed.fields[standbyFieldIndex] = {
+    ...embed.fields[standbyFieldIndex],
+    name: `Standby (${standbySize})`,
+    value: standbySize > 0 ? toMentions(standbyUsers) : 'None'
+  }
 
   return embed;
 }
@@ -38,31 +51,33 @@ const createQueueEmbed = async ({ message, title = 'Queue', date = new Date(Date
   const template = new MessageEmbed()
     .setTitle(title)
     .setDescription(description)
-    .setColor(startColor);
+    .setColor(startColor)
+    .addField('Confirmed (0)', 'None')
+    .addField('Standby (0)', 'None');
 
-  try {
-    const embedMessage = await message.embed(template);
+  const embedMessage = await message.embed(template);
 
-    const filter = (reaction) => EMOJIS.includes(reaction.emoji.name);
-    const collector = embedMessage.createReactionCollector(filter, { dispose: true, time: TWO_WEEKS_MS });
-    
-    collector.on('collect', (reaction) => {
-      reaction.message.edit('', { embed: updateEmbed({ template, reactions: embedMessage.reactions.cache, partySize }) });
-    });
+  const filter = (reaction) => EMOJIS.includes(reaction.emoji.name);
+  const collector = embedMessage.createReactionCollector(filter, { dispose: true, time: TWO_WEEKS_MS });
+  
+  collector.on('collect', (reaction, user) => {
+    if (user.bot)
+      return;
 
-    collector.on('remove', (reaction) => {
-      reaction.message.edit('', { embed: updateEmbed({ template, reactions: embedMessage.reactions.cache, partySize }) });
-    });
+    reaction.message.edit('', { embed: updateEmbed({ template, reactions: embedMessage.reactions.cache, partySize }) });
+  });
 
-    await embedMessage.react(Q);
-    await embedMessage.react(S);
+  collector.on('remove', (reaction, user) => {
+    if (user.bot)
+      return;
 
-    return embedMessage;
-  } catch (err) {
-    console.error(err);
-  }
+    reaction.message.edit('', { embed: updateEmbed({ template, reactions: embedMessage.reactions.cache, partySize }) });
+  });
 
-  return message;
+  await embedMessage.react(C);
+  await embedMessage.react(S);
+
+  return embedMessage;
 }
 
 module.exports = {
